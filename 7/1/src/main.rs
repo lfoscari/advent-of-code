353e8c30da49646a261c64d378f68626420f7b0e
Luigi Foscari <luigi.foscari@icloud.com>
2021-12-07 17:42:12 +0100

    seventh day

---
+++ 7/1/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+fn error(xs: &Vec<i32>, center: i32) -> i32 {
+    xs.iter().map(|&x| (x - center).abs()).sum()
+}
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Ops.");
+    let positions: Vec<i32> = contents.split(",").map(|s| s.parse().unwrap()).collect();
+
+    let (min, max) = (*positions.iter().min().unwrap(), *positions.iter().max().unwrap());
+    let min_error = (min..=max).map(|center| error(&positions, center)).min().unwrap();
+
+    println!("{:?}", min_error);
+}

ffb2ea9ea6e6bab86f44fb33d1667fb79c3fe026
Luigi Foscari <luigi.foscari@icloud.com>
2021-12-28 15:20:27 +0100

    fifteenth day (wip)

--- 15/1/src/main.rs
+++ 15/1/src/main.rs
@@ -1,23 +1,21 @@
 use std::fs;
-use std::cmp::min;
 
-fn find_path(from: (usize, usize), risks: &Vec<Vec<i32>>, path: &mut Vec<Vec<bool>>) {
-    let (i, j) = from;
-    path[i][j] = true;
-
-    match from {
-        (0, 0) => println!("Done"),
-        (i, 0) => find_path((i - 1, 0), risks, path),
-        (0, j) => find_path((0, j - 1), risks, path),
-        (i, j) => {
-            if risks[i - 1][j] <= risks[i][j - 1] {
-                find_path((i - 1, j), risks, path)
-            } else {
-                find_path((i, j - 1), risks, path)
-            }
-        }
-    }
-}
+// fn find_path(from: (usize, usize), risks: &Vec<Vec<i32>>, path: &mut Vec<Vec<bool>>) {
+//     let (i, j) = from;
+//     path[i][j] = true;
+//     match from {
+//         (0, 0) => return,
+//         (i, 0) => find_path((i - 1, 0), risks, path),
+//         (0, j) => find_path((0, j - 1), risks, path),
+//         (i, j) => {
+//             if risks[i - 1][j] <= risks[i][j - 1] {
+//                 find_path((i - 1, j), risks, path)
+//             } else {
+//                 find_path((i, j - 1), risks, path)
+//             }
+//         }
+//     }
+// }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Input file not found");
@@ -33,39 +31,41 @@ fn main() {
         .collect();
 
     let size = grid.len();
-    let mut risks: Vec<Vec<i32>> = (0..size).map(|_| (0..size as i32).collect()).collect();
 
-    for i in 0..size {
-        for j in 0..size {
-            match (i, j) {
-                (0, 0) => continue,
-                (0, _) => {
-                    risks[i][j] = grid[i][j] + risks[0][j - 1]
-                },
-                (_, 0) => {
-                    risks[i][j] = grid[i][j] + risks[i - 1][0]
-                },
-                (_, _) => {
-                    risks[i][j] = grid[i][j] + min(risks[i - 1][j], risks[i][j - 1])
-                }
-            }
-        }
-    }
+    // Dynamic programming
 
-    let mut path: Vec<Vec<bool>> = (0..size).map(|_| (0..size).map(|_| false).collect()).collect();
-    find_path((size - 1, size - 1), &risks, &mut path);
+    // let mut risks: Vec<Vec<i32>> = (0..size).map(|_| (0..size).map(|_| 0).collect()).collect();
 
-    for i in 0..size {
-        for j in 0..size {
-            if path[i][j] {
-                print!("[{}]", grid[i][j]);
-            } else {
-                print!(" {} ", grid[i][j]);
-            }
-        }
-        println!("");
-    }
+    // for i in 0..size {
+    //     for j in 0..size {
+    //         risks[i][j] = match (i, j) {
+    //             (0, 0) => 0,
+    //             (0, _) => grid[i][j] + risks[0][j - 1],
+    //             (_, 0) => grid[i][j] + risks[i - 1][0],
+    //             (_, _) => grid[i][j] + risks[i - 1][j].min(risks[i][j - 1])
+    //         }
+    //     }
+    // }
 
-    // It's dynamic programming, yeah...
-    println!("Risk: {:?}", risks[size - 1][size - 1]);
+    // let mut path: Vec<Vec<bool>> = (0..size).map(|_| (0..size).map(|_| false).collect()).collect();
+    // find_path((size - 1, size - 1), &risks, &mut path);
+
+    // for i in 0..size {
+    //     for j in 0..size {
+    //         if path[i][j] {
+    //             print!("[{}]", grid[i][j]);
+    //         } else {
+    //             print!(" {} ", grid[i][j]);
+    //         }
+    //     }
+    //     println!("");
+    // }
+
+    // println!("Risk: {:?}", risks[size - 1][size - 1]);
+
+    // Dijkstra algorithm
+
+    
+
+    println!("{:?}", risks);
 }
